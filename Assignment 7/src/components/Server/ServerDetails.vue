894f80aab0daaea2afed1ac22bcac683f5789279
MarkusNyland <nylmar15@student.westerdals.no>
2019-01-31 11:04:47 +0100

    Init

---
+++ Assignment 7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Please select a server</p>
+        <p v-else>Server #{{server.id}} selected, Status: {{server.status}}</p>
+        <hr>
+        <button @click="changeToNormal">Change server status to Normal</button>
+    </div>
+
+</template>
+
+<script>
+    import {EventBus} from "../../main";
+
+    export default {
+        data() {
+            return {
+                server: null
+            }
+        },
+        methods: {
+            changeToNormal(){
+                this.server.status = 'Normal'
+            }
+        },
+        created() {
+            EventBus.$on('loadServer', (server) => {
+                this.server = server;
+            })
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Assignment 7/src/components/Server/SingleServer.vue
@@ -0,0 +1,21 @@
+<template>
+    <li class="list-group-item">
+        Server #{{ server.id }}
+        <button @click="loadServerDetails">Load Server Details</button>
+    </li>
+</template>
+
+<script>
+    import {EventBus} from "../../main";
+
+    export default {
+        props: {
+            server: Object
+        },
+        methods:{
+            loadServerDetails(){
+                EventBus.$emit('loadServer', this.server);
+            }
+        }
+    }
+</script>
